e9e8c4eb702deb501de4d2abbaa4211713850796
Osmar Reyes <osmarreyesst@gmail.com>
2020-08-19 16:07:03 -0500

    add icons

--- src/components/Quad/index.js
+++ src/components/Quad/index.js
@@ -1,16 +1,20 @@
 import React, { useContext } from "react";
-import { StoreContext } from '../../context/store/storeContext'
+import { StoreContext } from '../../context/store/storeContext';
+import exImg from '../../assets/img/times-solid.svg';
+import circleImg  from '../../assets/img/circle-regular.svg';
 
 const Quad = (props) => {
 	const { state, actions } = useContext(StoreContext);
-	console.log(state)
+	//console.log(state)
 	const { setQuadValue } = actions.generalActions;
 	const quadValue = state.generalStates.quads[props.index];
 	const isTaken = quadValue !== 0;
 	return (
 		<div className="quad-body" onClick={() => { setQuadValue(props.index, state.generalStates.turn); }}>
 			{ isTaken
-				? (<div className="quad-icon">{quadValue}</div>)
+				? (<div className="quad-icon">
+					<img src={ quadValue === 'x' ? exImg : circleImg} />
+				</div>)
 				: ''
 			}
 		</div>
