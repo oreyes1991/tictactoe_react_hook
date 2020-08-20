ad43f7efd0e8645a3cdc48898ed6688ad3a7ca99
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-20 18:29:06 +0000

    Deploy oreyes1991/tictactoe_react_hook to github.com/oreyes1991/tictactoe_react_hook.git:gh-pages

---
+++ precache-manifest.1ae6f8e33965fb2b6b7dd07f8b744770.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c67d2e0ae22912e533c6b11927a43c5",
+    "url": "/tictactoe_react_hook/index.html"
+  },
+  {
+    "revision": "222accbb2335fd86cf92",
+    "url": "/tictactoe_react_hook/static/css/main.0a0bf738.chunk.css"
+  },
+  {
+    "revision": "2266249f276c1184ba23",
+    "url": "/tictactoe_react_hook/static/js/2.ca1056ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe_react_hook/static/js/2.ca1056ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "222accbb2335fd86cf92",
+    "url": "/tictactoe_react_hook/static/js/main.731d7a90.chunk.js"
+  },
+  {
+    "revision": "c02547f28d0e6099ad00",
+    "url": "/tictactoe_react_hook/static/js/runtime-main.57aa9242.js"
+  },
+  {
+    "revision": "e9c99b07456f5051ba9f0aef445ea84b",
+    "url": "/tictactoe_react_hook/static/media/circle-regular.e9c99b07.svg"
+  },
+  {
+    "revision": "f0ebab4ba23d25e3a2e35c98825c5008",
+    "url": "/tictactoe_react_hook/static/media/times-solid.f0ebab4b.svg"
+  }
+]);
